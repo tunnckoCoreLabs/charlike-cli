43739de5ebbfbd9924a0c9c243e972f45b3b24c6
tunnckoCore <mameto_100@mail.bg>
2016-12-10 02:44:52 +0200

    chore(init): push

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * charlike-cli <https://github.com/tunnckoCore/charlike-cli>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function charlikeCli () {
+
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * charlike-cli <https://github.com/tunnckoCore/charlike-cli>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+const test = require('mukla')
+const charlikeCli = require('./index')
+
+test('charlike-cli', function (done) {
+  charlikeCli()
+  done()
+})
